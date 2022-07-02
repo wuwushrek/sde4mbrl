981ad41b767be0ea651ca66878ab41b630deab6a
Franck Djeumou <frmbouwe@gmail.com>
2022-07-01 18:00:34 -0700

    fix repo name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
    long_description=long_description,
    author='Franck Djeumou',
    author_email='fdjeumou@utexas.edu',
-   url="https://github.com/wuwushrek/rl-girsanov.git",
+   url="https://github.com/wuwushrek/rl_girsanov.git",
    packages=['rl_girsanov'],
    package_dir={'rl_girsanov': 'rl_girsanov/'},
    install_requires=['numpy', 'scipy', 'matplotlib', 'tqdm', 'jax>=0.3.4', 'jaxlib', 'dm-haiku', 'optax'],
