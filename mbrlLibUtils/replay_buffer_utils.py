Updating ensemble propagation methods and fixing off-by-one indexing of ensemble trajectory rollouts.

mbrlLibUtils/replay_buffer_utils.py
@@ -83,12 +83,14 @@ def generate_sample_trajectories(init_state, num_particles, dynamics_model, gene
 
     # act = torch.zeros((num_particles,0), device=device)
 
+    sample_trajectories = model_state['obs'].reshape(num_particles, 1, -1)
+
     for t in range(time_horizon):
         with torch.no_grad():
             next_obs, _, _, model_state = dynamics_model.sample(ufun(model_state), model_state, rng=generator)
-        if t == 0:
-            sample_trajectories = next_obs.reshape(num_particles, 1, -1)
-        else:
-            sample_trajectories = torch.concatenate((sample_trajectories, next_obs.reshape(num_particles, 1, -1)), axis=1)
+        # if t == 0:
+        #     sample_trajectories = torch.concatenate((initial_obs_batch.reshape(num_particles, 1, -1), next_obs.reshape(num_particles, 1, -1)), axis=1) #next_obs.reshape(num_particles, 1, -1)
+        # else:
+        sample_trajectories = torch.concatenate((sample_trajectories, next_obs.reshape(num_particles, 1, -1)), axis=1)
 
     return sample_trajectories

mbrlLibUtils/save_and_load_models.py
@@ -71,7 +71,7 @@ def load_model_and_config(save_dir, propagation_method=None):
 
     return model, config
 
-def load_learned_ensemble_model(model_path, horizon=1, num_samples=1, ufun=None, rseed=1000, prior_dist=False):
+def load_learned_ensemble_model(model_path, horizon=1, num_samples=1, ufun=None, propagation_method="fixed_model", rseed=1000, prior_dist=False):
     """ Load the learned model from the path
 
         Args:
@@ -79,6 +79,15 @@ def load_learned_ensemble_model(model_path, horizon=1, num_samples=1, ufun=None,
             horizon (int, optional): The horizon of the model
             num_samples (int, optional): The number of samples to generate
             ufun (function, optional): The control function
+            propagation_method (str, optional): The propagation method to used to generate trajectories.
+            Valid propagation options are:
+            - "random_model": for each output in the batch a model will be chosen at random.
+              This corresponds to TS1 propagation in the PETS paper.
+            - "fixed_model": for output j-th in the batch, the model will be chosen according to
+              the model index in `propagation_indices[j]`. This can be used to implement TSinf
+              propagation, described in the PETS paper.
+            - "expectation": the output for each element in the batch will be the mean across
+              models.
             rseed (int, optional): The random seed for the sampling (default: 1000)
             prior_dist (bool, optional): If True, the function will sample from the prior knowledge of the system + prior diffusion
 
@@ -90,7 +99,7 @@ def load_learned_ensemble_model(model_path, horizon=1, num_samples=1, ufun=None,
         
     """
     # Load the model
-    model, _ = load_model_and_config(model_path, propagation_method="expectation")
+    model, _ = load_model_and_config(model_path, propagation_method=propagation_method)
 
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -101,7 +110,7 @@ def load_learned_ensemble_model(model_path, horizon=1, num_samples=1, ufun=None,
     generator.manual_seed(rseed)
 
     def sampling(y, rng):
-        return generate_sample_trajectories(y, num_samples, model, generator, time_horizon=horizon + 1, ufun=ufun, device=device).cpu().numpy()
+        return generate_sample_trajectories(y, num_samples, model, generator, time_horizon=horizon, ufun=ufun, device=device).cpu().numpy()
 
     _time_evol = np.arange(0, horizon + 1, 0.01)
 

sde4mbrlExamples/mass_spring_damper/pred_analysis_script.py
@@ -271,7 +271,7 @@ def create_uncertainty_plots(cfg_path, learned_dir, data_dir, gt_dir):
         if 'gaussian_mlp_ensemble' in model_name:
             _mesh_learned_model, _ = load_learned_ensemble_model(learned_dir+model_name, horizon=horizon_pred_accuracy, 
                                                         num_samples=num_particles if 'node' not in model_name else 1, 
-                                                        ufun=None)
+                                                        ufun=None, propagation_method='fixed_model')
         else:
             # Now let's create a model to compute prediction error and std on the meshgrid
             # TODO: Make sure load learn model works for probabilistic models too
@@ -300,8 +300,8 @@ def create_uncertainty_plots(cfg_path, learned_dir, data_dir, gt_dir):
         itr_count += 1
     
     # Let's compute the maximum error
-    _max_error = np.max(_mesh_error_evol)
-    # _max_error = 0.2
+    # _max_error = np.max(_mesh_error_evol)
+    _max_error = 0.2
         
     # Now, let's plot the results
     # Loop over the files
@@ -464,7 +464,7 @@ def create_state_prediction(cfg_path, learned_dir, data_dir, gt_dir):
             # TODO: Make sure load learn model works for probabilistic models too
             # _mesh_learned_model, _my_time = load_learned_model(learned_dir+model_name, horizon=horizon_accuracy, num_samples=num_particles, ufun=None)
             if 'gaussian_mlp_ensemble' in model_name:
-                _mesh_learned_model, _ = load_learned_ensemble_model(learned_dir+model_name, horizon=horizon_accuracy, num_samples=num_particles, ufun=None)
+                _mesh_learned_model, _ = load_learned_ensemble_model(learned_dir+model_name, horizon=horizon_accuracy, num_samples=num_particles, ufun=None,  propagation_method='fixed_model')
             else:
                 # Now let's create a model to compute prediction error and std on the meshgrid
                 # TODO: Make sure load learn model works for probabilistic models too